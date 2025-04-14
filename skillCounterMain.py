0ffa98d0ffb057e7fd037313626ef7f32b3ea4bd
ThatBananer <30753385+Jon-1162@users.noreply.github.com>
2025-04-13 21:19:53 -0400

    Extracted skills from stack overflow survey data

--- skillCounterMain.py
+++ skillCounterMain.py
@@ -12,7 +12,7 @@ import time
 
 
 # linkedInURL = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=SWE&location=Las%20Vegas,%20Nevada,%20United%20States&geoId=100293800&currentJobId=3415227738&position=1&pageNum=0&start=25"
-
+skillSet = {'Rust', 'PowerShell', 'Linode', 'Akamai', 'OpenGL', 'WordPress', 'Keras', 'ASP.NET CORE', 'Webpack', 'Lua', 'Unreal Engine', 'Asana', '.NET', 'Eclipse', 'Oracle Cloud Infrastructure (OCI)', 'ZMK', 'Fastify', 'Pandas', 'Code::Blocks', 'Jira', 'PHP', 'Elm', 'GDScript', 'Cobol', 'OpenShift', 'Electron', 'doctest', 'OpenCL', 'cppunit', 'Visual Studio Code', 'Nix', 'Basecamp', 'Go', 'CodeIgniter', 'Bun', 'Kotlin', 'Drupal', 'Gradle', 'Netbeans', 'MariaDB', 'Solr', 'Astro', 'Strapi', 'WebStorm', 'Capacitor', 'Oracle', 'Fortran', 'Geany', 'Xcode', 'Python', 'Catch2', 'InfluxDB', 'Hadoop', 'Couch DB', 'Microsoft Lists', 'MSBuild', 'Yii 2', 'Erlang', 'Nim', 'Visual Basic (.Net)', 'Firebase', 'MATLAB', 'Boost.Test', 'Azure Devops', 'VSCodium', 'PyCharm', 'CLion', 'PostgreSQL', 'Puppet', 'Nano', 'Next.js', 'MongoDB', 'Spacemacs', 'Neo4J', 'Swift', 'Visual Studio Solution', 'AngularJS', 'Firebird', 'Stack Overflow for Teams', 'Linear', 'C', 'mlflow', 'Vue.js', 'VBA', 'Scala', 'Flutter', 'PythonAnywhere', 'Cargo', 'Chocolatey', 'Node.js', 'Podman', 'Fly.io', 'build2', 'PhpStorm', 'Dagger', 'Clickup', 'Julia', 'SwiftUI', 'Neovim', 'Elasticsearch', 'Composer', 'C++', 'Scikit-Learn', 'pnpm', 'Supabase', 'MySQL', 'Flask', 'Elixir', 'EventStoreDB', 'C#', 'Amazon Web Services (AWS)', 'JavaScript', 'Google Test', 'Homebrew', 'Terraform', 'Torch/PyTorch', 'Vim', 'Lucid', 'Zephyr', 'Spring Boot', 'NestJS', "LLVM's Clang", 'ASP.NET', 'RubyMine', '.NET MAUI', 'Couchbase', 'HTML/CSS', 'Presto', 'Solid.js', 'Spring Framework', 'TensorFlow', 'Android Studio', 'Goland', 'Doxygen', 'Qt Creator', 'Spyder', 'GNU GCC', 'Ruby', 'Micronaut', 'GTK', 'RabbitMQ', 'Rider', 'Notepad++', 'Prolog', 'Opencv', 'Deno', 'Rad Studio (Delphi, C++ Builder)', 'Assembly', 'Svelte', 'GitHub Discussions', 'Shortcut', 'Delphi', 'Vultr', 'React Native', 'Solidity', 'Cosmos DB', 'React', 'SCons', 'JAX', 'Unity 3D', 'Jupyter Notebook/JupyterLab','Snowflake', 'Tauri', 'Godot', 'Cloudflare', 'Symfony', 'Arduino', 'Helix', 'Crystal', 'F#', 'SQLite', 'DataGrip', 'BBEdit', 'MFC', 'RavenDB', 'Digital Ocean', 'npm', 'Apex', 'Confluence', 'Render', '.NET Framework (1.0 - 4.8)', 'jQuery', 'Pacman', 'H2', 'Phoenix', 'Ruby on Rails', 'Ada', 'CUDA', 'Yarn', 'Vercel', 'R', 'Java', 'Blazor', 'Xamarin', 'Sublime Text', 'OpenStack', 'Groovy', 'Colocation', 'Miro', 'Gatsby', 'Clickhouse', 'Obsidian', 'Cordova', 'Meson', 'Chef', 'NuGet', 'Nuxt.js', 'SQL', 'Tidyverse', 'IBM Cloud Or Watson', 'YouTrack', 'Google Cloud', 'Alibaba Cloud', 'Cassandra', 'QMake', 'Qt', 'Microsoft SQL Server', 'Dart', 'NumPy', 'Bash/Shell (all shells)', 'Play Framework', 'Redis', 'Htmx', 'RStudio', 'Markdown File', 'Roslyn', 'Haskell', 'Ninja', 'Airtable', 'Ktor', 'Hetzner', 'Laravel', 'Pip', 'Heroku', 'Express', 'deleteMeLater', 'Datomic', 'FastAPI', 'Docker', 'PlatformIO', 'Ansible', 'Remix', 'Angular', 'IBM DB2', 'CMake', 'Dynamodb', 'Kate', 'Lisp', 'Zig', 'Monday.com', 'CUTE', 'Pulumi', 'Trello', 'OVH', 'Apache Kafka', 'Emacs', 'Cloud Firestore', 'TypeScript', 'Microsoft Access', 'DuckDB', 'DirectX', 'Quarkus', 'IntelliJ IDEA', 'Notion', 'Perl', 'Clojure', 'Rasberry Pi', 'Managed Hosting', 'Databricks', 'Apache Spark', 'Hugging Face Transformers', 'Vite', 'Databricks SQL', 'APT', 'Ionic', 'VMware', 'OCaml', 'Ant', 'Kubernetes', 'Visual Studio', 'MicroPython', 'Scaleway', 'MSVC', 'Microsoft Planner', 'Make', 'IPython', 'Cockroachdb', 'Wikis', 'TiDB', 'Django', 'Redmine', 'Smartsheet', 'Firebase Realtime Database', 'BigQuery', 'Maven (build tool)', 'Ruff', 'Microsoft Azure', 'Fleet', 'Netlify', 'Objective-C'}
 
 l=[]
 o={}
@@ -56,33 +56,3 @@ df = pd.DataFrame(k)
 df.to_csv('linkedinjobs.csv', index=False, encoding='utf-8')
 print(k)
 
-
-
-# for j in range(0,len(l)):
-
-#     resp = requests.get(target_url.format(l[j]))
-#     soup=BeautifulSoup(resp.text,'html.parser')
-
-#     try:
-#         o["company"]=soup.find("div",{"class":"top-card-layout__card"}).find("a").find("img").get('alt')
-#     except:
-#         o["company"]=None
-
-#     try:
-#         o["job-title"]=soup.find("div",{"class":"top-card-layout__entity-info"}).find("a").text.strip()
-#     except:
-#         o["job-title"]=None
-
-#     try:
-#         o["level"]=soup.find("ul",{"class":"description__job-criteria-list"}).find("li").text.replace("Seniority level","").strip()
-#     except:
-#         o["level"]=None
-
-
-
-#     k.append(o)
-#     o={}
-
-# df = pd.DataFrame(k)
-# df.to_csv('linkedinjobs.csv', index=False, encoding='utf-8')
-# print(k)
